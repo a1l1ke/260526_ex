d90d1194076619325b124732eae273268e22428d
a1l1ke <onetenthplease@gmail.com>
2026-05-26 15:47:34 +0900

    Class static 필드/메서드 실습 및 예외처리 실습 코드 추가

--- 07_class2.js
+++ 07_class2.js
@@ -1,8 +1,17 @@
 // 상속
 class Animal {
+  count = 0; // 인스턴스 귀속 -> new 만들면 그 객체에서 count 영역이 생김
+  static count = 0; // 클래스 귀속 -> 별도 저장공간에 class 설계와 함께 저장되어 있음
+  static moreCount() {
+    Animal.count++;
+  }
   #name;
   constructor(name) {
     this.#name = name;
+    this.count++;
+    // Animal.count++;
+    Animal.moreCount(); // 함수도 클래스 static
+    // 클래스/인스턴스에 속한 함수는 메서드. -> static 메서드
   }
   speak() {
     console.log(this.#name);
@@ -18,6 +27,7 @@ class Cat extends Animal {
     // private -> 상속을 받아도 그 속성에 접근할 수 X.
     super(name);
     this.#age = age;
+    console.log("count", Animal.count);
   }
   speak() {
     super.speak(); // 직전 상속 받은 원본의 것을 super로 쓰면서
@@ -30,3 +40,8 @@ const c = new Cat("비스마르크", 50);
 a.speak();
 b.speak();
 c.speak();
+console.log(a.count, b.count, c.count);
+console.log(Animal.count);
+// Static - 인스턴스와 다른 저장 공간이므로 이름 겹쳐도 된다
+// 클래스.(이름). 변수/함수 다 된다
+// 쓸 일 있으면 class.() 쓰면 된다
